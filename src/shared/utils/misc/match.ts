afac59d6eea323ff1f75cdc2538954c647331718
Ivan Zorin <creator@localzet.com>
2025-06-21 22:29:34 +0300

    Update utils

--- src/shared/utils/misc/match.ts
+++ src/shared/utils/misc/match.ts
@@ -1,9 +1,9 @@
 import invariant from 'tiny-invariant'
 
 /**
- * PHP like match function
- * @param conditions - Array of [condition, value]
- * @returns Value of first met condition
+ * PHP-подобная функция match
+ * @param conditions - Массив из [условие, значение]
+ * @returns Значение первого выполненного условия
  * @example
  * ```tsx
  * const value = match(
@@ -15,6 +15,6 @@ import invariant from 'tiny-invariant'
  */
 export function match<T>(...conditions: Array<[boolean, T]>) {
     const foundedCondition = conditions.find(([condition]) => condition) ?? conditions.at(-1)
-    invariant(foundedCondition, 'No conditions have been met')
+    invariant(foundedCondition, 'Ни одно из условий не выполнено')
     return foundedCondition[1]
 }
